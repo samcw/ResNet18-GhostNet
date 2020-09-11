fc4a22bcf1b4e01717d49afb8b3d2c417ba5f43c
Histo <tsukihie@gmail.com>
2020-09-11 21:09:42 +0800

    init project

---
+++ run.py
@@ -0,0 +1,89 @@
+from resnet18 import ResNet18
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+import torch
+import torch.nn as nn
+
+#check gpu
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+#set hyperparameter
+EPOCH = 10
+pre_epoch = 0
+BATCH_SIZE = 128
+LR = 0.01
+
+if __name__ == '__main__':
+    # prepare dataset and preprocessing
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+    ])
+
+    trainset = torchvision.datasets.CIFAR10(root='../data', train=True, download=True, transform=transform_train)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)
+
+    testset = torchvision.datasets.CIFAR10(root='../data', train=False, download=True, transform=transform_test)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+
+    # labels in CIFAR10
+    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    # define ResNet18
+    net = ResNet18().to(device)
+
+    # define loss funtion & optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=LR, momentum=0.9, weight_decay=5e-4)
+    # train
+    for epoch in range(pre_epoch, EPOCH):
+        print('\nEpoch: %d' % (epoch + 1))
+        net.train()
+        sum_loss = 0.0
+        correct = 0.0
+        total = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # prepare dataset
+            length = len(trainloader)
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
+            optimizer.zero_grad()
+
+            # forward & backward
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print ac & loss in each batch
+            sum_loss += loss.item()
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += predicted.eq(labels.data).cpu().sum()
+            print('[epoch:%d, iter:%d] Loss: %.03f | Acc: %.3f%% '
+                  % (epoch + 1, (i + 1 + epoch * length), sum_loss / (i + 1), 100. * correct / total))
+
+        # get the ac with testdataset in each epoch
+        print('Waiting Test...')
+        with torch.no_grad():
+            correct = 0
+            total = 0
+            for data in testloader:
+                net.eval()
+                images, labels = data
+                images, labels = images.to(device), labels.to(device)
+                outputs = net(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum()
+            print('Test\'s ac is: %.3f%%' % (100 * correct / total))
+
+    print('Train has finished, total epoch is %d' % EPOCH)
