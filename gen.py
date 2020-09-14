f01a26bd8eb313d7b1ba0093c4874ba418a0b48e
Histo <tsukihie@gmail.com>
2020-09-14 20:54:25 +0800

    add senet

--- gen.py
+++ gen.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 withoutGhost = []
 withGhost = []
+withSwish = []
 epoch = []
 
 for i in range(50):
@@ -10,6 +11,7 @@ for i in range(50):
 
 dataNotWithGhost = pd.read_csv('./with.csv', usecols=[0])
 dataWithGhost = pd.read_csv('./without.csv', usecols=[0])
+dataWithSwish = pd.read_csv('./withswish.csv', usecols=[0])
 
 for index, row in dataNotWithGhost.iterrows():
     withoutGhost.append(int(row['with'][7:9]))
@@ -17,10 +19,14 @@ for index, row in dataNotWithGhost.iterrows():
 for index, row in dataWithGhost.iterrows():
     withGhost.append(int(row['without'][7:9]))
 
+for index, row in dataWithSwish.iterrows():
+    withSwish.append(int(row['with'][7:9]))
+
 print(epoch)
 
 plt.plot(epoch, withoutGhost, color='r', label='withoutGhost')
 plt.plot(epoch, withGhost, color=(0, 0, 0), label='withGhost')
+plt.plot(epoch, withSwish, color='b', label='withSwish')
 
 plt.xlabel('Epochs')
 plt.ylabel('Acc')
